2519dd40c7441b8987b887934d151a6ba58aab61
Marcus Augustus Fernandes Guimaraes <marcustusgui@gmail.com>
2025-01-08 19:54:48 +0000

    Add new file

---
+++ eletronica/sensoriamento/SensordeNivel.ino
@@ -0,0 +1,25 @@
+// Define o pino ao qual o sensor está conectado
+const int pinSensor = 2;  // Pino digital conectado ao sensor
+
+void setup() {
+  // Configura o pino do sensor como entrada
+  pinMode(pinSensor, INPUT);
+
+  // Inicializa a comunicação serial para monitorar o estado do sensor
+  Serial.begin(9600);
+}
+
+void loop() {
+  // Lê o estado do sensor (HIGH ou LOW)
+  int estadoSensor = digitalRead(pinSensor);
+
+  // Verifica o estado do sensor e exibe no monitor serial
+  if (estadoSensor == HIGH) {
+    Serial.println("Nível de água baixo");
+  } else {
+    Serial.println("Nível de água alto");
+  }
+
+  // Aguarda 500 milissegundos antes de verificar novamente
+  delay(500);
+}
