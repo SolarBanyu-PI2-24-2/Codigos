b24a70ef3c68d6c55748d6ecadbbf9da84e13a66
DouglasMonteles <silvadouglas328@gmail.com>
2025-01-11 00:54:09 -0300

    :sparkles: adicionado alguns tópicos de teste
    Change-Id: I71bab17fa85f3d5e225d83363b2167d2070b84c0

--- software/embarcado/djangoapp/mqttapp/mqtt_client.py
+++ software/embarcado/djangoapp/mqttapp/mqtt_client.py
@@ -1,20 +1,34 @@
 import paho.mqtt.client as mqtt
+import environ
 
-BROKER = "test.mosquitto.org"
-PORT = 1883  # Porta MQTT padrão
-TOPIC = "test/mqtt/django"
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+DATA_DIR = BASE_DIR.parent / 'data' / 'web'
+
+env = environ.Env()
+ENV_FILE = BASE_DIR / ".env"
+env.read_env(ENV_FILE)
+
+BROKER_HOST = env('BROKER_HOST')
+BROKER_PORT = env.int('BROKER_PORT')
+TOPICS = env('BROKER_TOPICS').split(',')
 
 # Callback executado quando a conexão com o broker é estabelecida
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
         print("Conectado ao broker MQTT com sucesso!")
-        client.subscribe(TOPIC)
-        print(f"Inscrito no tópico: {TOPIC}")
+        
+        for topic in TOPICS:
+            client.subscribe(str(topic))
+        
+        print(f"Inscrito nos tópicos: {TOPICS}")
     else:
         print(f"Erro ao conectar ao broker. Código: {rc}")
 
 # Callback executado quando uma mensagem é recebida
 def on_message(client, userdata, msg):
+    # TODO: send to backend
     print(f"Mensagem recebida no tópico {msg.topic}: {msg.payload.decode()}")
 
 # Inicializa e configura o cliente MQTT
@@ -23,5 +37,5 @@ mqtt_client.on_connect = on_connect
 mqtt_client.on_message = on_message
 
 def start_mqtt():
-    mqtt_client.connect(BROKER, PORT)
-    mqtt_client.loop_start()  # Mantém o cliente rodando em segundo plano
+    mqtt_client.connect(BROKER_HOST, BROKER_PORT)
+    mqtt_client.loop_start()
