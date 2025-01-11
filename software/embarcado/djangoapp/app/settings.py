7cb821d76e54230f0060df6c16dc4bfd9adf2dcf
DouglasMonteles <silvadouglas328@gmail.com>
2025-01-10 23:18:24 -0300

    :building_construction: configurado o docker e docker-compose
    Change-Id: I0ce2d3d9d19e48026e5db019bc1e2376fc72bbf3

--- software/embarcado/djangoapp/app/settings.py
+++ software/embarcado/djangoapp/app/settings.py
@@ -10,23 +10,29 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import environ
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+DATA_DIR = BASE_DIR.parent / 'data' / 'web'
 
+env = environ.Env()
+ENV_FILE = BASE_DIR / ".env"
+env.read_env(ENV_FILE)
+
+SECRET_KEY = env("SECRET_KEY", default="django-insecure-#123!@#$%^&*()_+")
+
+DEBUG = env.bool("DEBUG", default=False)
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ez35t0@tl!egntp_o0$am-fab*n^i9ri2-(rvlq@m)pbn8dcbw'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    h.strip() for h in env("ALLOWED_HOSTS", default="localhost, 127.0.0.1").split(',')
+    if h.strip()
+]
 
 # Application definition
 
@@ -36,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'mqttapp',
 ]
 
