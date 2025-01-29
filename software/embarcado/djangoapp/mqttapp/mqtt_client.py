e006688bed0b1aa25a82dfdef2ee3c4bb4a3f5fe
DouglasMonteles <silvadouglas328@gmail.com>
2025-01-29 15:13:37 -0300

    enviando dados estáticos para o backend

--- software/embarcado/djangoapp/mqttapp/mqtt_client.py
+++ software/embarcado/djangoapp/mqttapp/mqtt_client.py
@@ -1,5 +1,6 @@
 import paho.mqtt.client as mqtt
 import environ
+import requests
 
 from pathlib import Path
 
@@ -12,30 +13,44 @@ env.read_env(ENV_FILE)
 
 BROKER_HOST = env('BROKER_HOST')
 BROKER_PORT = env.int('BROKER_PORT')
-TOPICS = env('BROKER_TOPICS').split(',')
+TOPIC = env('BROKER_TOPIC')
+API_HOST = env('API_HOST')
 
-# Callback executado quando a conexão com o broker é estabelecida
+sensors = []
+
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
         print("Conectado ao broker MQTT com sucesso!")
-        
-        for topic in TOPICS:
-            client.subscribe(str(topic))
-        
-        print(f"Inscrito nos tópicos: {TOPICS}")
+        client.subscribe(str(TOPIC))
+        print(f"Inscrito no tópico: {TOPIC}")
     else:
         print(f"Erro ao conectar ao broker. Código: {rc}")
 
-# Callback executado quando uma mensagem é recebida
 def on_message(client, userdata, msg):
-    # TODO: send to backend
     print(f"Mensagem recebida no tópico {msg.topic}: {msg.payload.decode()}")
+    
+    #TODO: pegar o tipo de sensor pelo payload da mensagem e filtrar o sensor
+    
+    headers = {"Content-Type": "application/json"}
+    dados = {
+        "valor": 12.43,
+        "unidade": "L",
+        "sensor_id": "0f9fc644-532c-48e1-b09e-54667e54eac9"
+    }
 
-# Inicializa e configura o cliente MQTT
+    response = requests.post(f"{API_HOST}/dados_sensores/", headers=headers, json=dados)
+    print("Status Code:", response.status_code)
+
 mqtt_client = mqtt.Client()
 mqtt_client.on_connect = on_connect
 mqtt_client.on_message = on_message
 
 def start_mqtt():
+    global sensors
+    response = requests.get(f"{API_HOST}/sensores/")
+    sensors = response.json()
+    
+    print(f"Sensores: {sensors}")
+    
     mqtt_client.connect(BROKER_HOST, BROKER_PORT)
     mqtt_client.loop_start()
