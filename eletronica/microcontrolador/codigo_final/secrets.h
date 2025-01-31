02e1769953c7304fef2fcfbaffaf0c12f6bef069
DouglasMonteles <silvadouglas328@gmail.com>
2025-01-31 16:53:41 -0300

    codigo final com wi-fi e mqtt

---
+++ eletronica/microcontrolador/codigo_final/secrets.h
@@ -0,0 +1,11 @@
+#define WIFI_SSID "nome_rede_wifi";
+#define WIFI_PASSWORD "senha_rede_wifi";
+
+#define MQTT_BROKER "s1.eu.hivemq.cloud";
+#define MQTT_PORT 8883;
+#define MQTT_USERNAME "usuario";
+#define MQTT_PASSWORD "senha";
+
+#define MQTT_PUBLISH_TOPIC "solarbanyu/sensors/values";
+
+#define PUB_EACH_10_SEC 10000;
