dbd6137e68f898c435aaf57bc0959032b5ff98d9
DouglasMonteles <silvadouglas328@gmail.com>
2025-01-30 23:23:55 -0300

    Enviando todos os dados dos sensores para a api
    Change-Id: I10f23a4048dd8641bb9020b697d30ccf976618b5

--- software/embarcado/djangoapp/mqttapp/mqtt_client.py
+++ software/embarcado/djangoapp/mqttapp/mqtt_client.py
@@ -1,7 +1,11 @@
-import paho.mqtt.client as mqtt
 import environ
 import requests
+import ssl
+import paho.mqtt.client as paho
+import paho.mqtt.subscribe as subscribe
+import json
 
+from paho import mqtt
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -13,37 +17,35 @@ env.read_env(ENV_FILE)
 
 BROKER_HOST = env('BROKER_HOST')
 BROKER_PORT = env.int('BROKER_PORT')
-TOPIC = env('BROKER_TOPIC')
+BROKER_USERNAME = env('BROKER_USERNAME')
+BROKER_PASSWORD = env('BROKER_PASSWORD')
+SENSORS_VALUES_TOPIC = env('BROKER_SENSOR_TOPIC')
+
 API_HOST = env('API_HOST')
 
 sensors = []
 
-def on_connect(client, userdata, flags, rc):
-    if rc == 0:
-        print("Conectado ao broker MQTT com sucesso!")
-        client.subscribe(str(TOPIC))
-        print(f"Inscrito no tópico: {TOPIC}")
-    else:
-        print(f"Erro ao conectar ao broker. Código: {rc}")
-
 def on_message(client, userdata, msg):
     print(f"Mensagem recebida no tópico {msg.topic}: {msg.payload.decode()}")
     
-    #TODO: pegar o tipo de sensor pelo payload da mensagem e filtrar o sensor
+    sensors_values = json.loads(msg.payload.decode())
     
     headers = {"Content-Type": "application/json"}
-    dados = {
-        "valor": 12.43,
-        "unidade": "L",
-        "sensor_id": "0f9fc644-532c-48e1-b09e-54667e54eac9"
-    }
+    
+    for sensor_value in sensors_values:
+        sensor_type = sensor_value["tipo"]
+        sensor = next((item for item in sensors if item["tipo"] == sensor_type), None)
+        
+        req_json = {
+            "valor": sensor_value["valor"],
+            "unidade": "%",
+            "sensor_id": sensor["id"]
+        }
 
-    response = requests.post(f"{API_HOST}/dados_sensores/", headers=headers, json=dados)
-    print("Status Code:", response.status_code)
-
-mqtt_client = mqtt.Client()
-mqtt_client.on_connect = on_connect
-mqtt_client.on_message = on_message
+        if sensor is not None:
+            print(f"Enviando: {sensor_value}")
+            response = requests.post(f"{API_HOST}/dados_sensores/", headers=headers, json=req_json)
+            print("Status Code:", response.status_code)
 
 def start_mqtt():
     global sensors
@@ -52,5 +54,16 @@ def start_mqtt():
     
     print(f"Sensores: {sensors}")
     
-    mqtt_client.connect(BROKER_HOST, BROKER_PORT)
-    mqtt_client.loop_start()
+    sslSettings = ssl.SSLContext(mqtt.client.ssl.PROTOCOL_TLS)
+    auth = {
+        'username': BROKER_USERNAME, 
+        'password': BROKER_PASSWORD,
+    }
+
+    subscribe.callback(on_message, 
+                SENSORS_VALUES_TOPIC, 
+                hostname=BROKER_HOST, 
+                port=BROKER_PORT, 
+                auth=auth,
+                tls=sslSettings, 
+                protocol=paho.MQTTv31)
