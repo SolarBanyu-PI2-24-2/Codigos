f81359b614f0bea6d6cb0a0394b00f9e10b617f3
Yasmim Rosa <mimsayrosa@gmail.com>
2025-02-15 19:44:33 -0300

    ajuste ALLOWED_HOSTS

--- software/backend/solarbanyu_app/solarbanyu_app/settings.py
+++ software/backend/solarbanyu_app/solarbanyu_app/settings.py
@@ -148,7 +148,7 @@ REST_FRAMEWORK = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000", "https://solarbanyu-backend.onrender.com/"
+    "http://localhost:3000"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
