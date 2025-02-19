89b91cda95cf7cf88eae556b03a37ab85b3e0634
Yasmim Rosa <mimsayrosa@gmail.com>
2025-02-19 13:25:16 -0300

    FIX: conn_max_age

--- software/backend/djangoapp/project/settings.py
+++ software/backend/djangoapp/project/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         default="postgresql://adm:BxbduNjS4aRWfRJF8AyZ5Pd4AY7MIdrA@dpg-cuotamaj1k6c739d8j8g-a.oregon-postgres.render.com/solarbanyu_db_lc23",
-        conn_max_age=120,
+        conn_max_age=40,
         conn_health_checks=True
     )
 }
